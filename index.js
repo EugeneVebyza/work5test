921be11ed8951cfcd0ef692747a5afc340758785
Eugene <zenya030201@gmail.com>
2022-04-19 12:08:23 +0400

    static

---
+++ index.js
@@ -0,0 +1,23 @@
+// class Shop {
+//   constructor(number, name, address) { 
+//     this.number = number;
+//     this.name = name;
+//     this.address = address;
+//   }
+
+//   set(number, name, address) { 
+//     this.number = number;
+//     this.name = name;
+//     this.address = address;
+//     this.render();
+//   }
+
+//   get(){
+
+//   }
+
+//   render(){
+
+//   }
+
+// }
